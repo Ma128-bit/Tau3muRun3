877691331c30b6ba03af309262110c70432d794a
fsimone91 <federica.maria.simone@cern.ch>
2025-01-29 16:47:54 +0100

    adding datacards and workspaces

---
+++ Combine/datacards_2023/run_subc.sh
@@ -0,0 +1,9 @@
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_A1.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_B1.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_C1.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_A2.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_B2.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_C2.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_A3.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_B3.txt --cl 0.90 -m 1.777
+combineTool.py -M AsymptoticLimits --run blind -d CMS_T3MSignal_13TeV_C3.txt --cl 0.90 -m 1.777
